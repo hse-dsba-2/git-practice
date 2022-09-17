874f993f12172ccd53796d0e14b132152ff24f48
Rudakov Kirill <rudakovkirillx@gmail.com>
2022-09-17 12:16:02 +0300

    add writer to file

--- generator.py
+++ generator.py
@@ -1,5 +1,9 @@
-def hello():
-    pass
-
+from string import ascii_letters
+import random
 if __name__ == "__main__":
-    print(__name__)
+    grades = open("grades.csv","w")
+    for i in range(10):
+        rand_int = [str(random.randrange(0, 11)) for i in range(10)]
+        rand_seq = random.choices(ascii_letters, k=random.randrange(3, 10))
+        grades.write("".join(rand_seq)+','+','.join(rand_int)+"\n")
+    grades.close()
