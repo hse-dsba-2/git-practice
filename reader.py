f772a8de9f1de386d2df283077621903ea6e1328
Rudakov Kirill <rudakovkirillx@gmail.com>
2022-09-20 10:49:39 +0300

    add reader

--- reader.py
+++ reader.py
@@ -5,3 +5,10 @@ with open("grades.csv", "r") as file:
         sum_grades = sum_grades[1:]
         sum_grades = sum(map(int, sum_grades))
         print(name, sum_grades)
+
+        # mean grade
+        # median grade
+        # max grade with name
+        # min grade and name
+        # names with greater grade than mean
+        # name with less grade than median
